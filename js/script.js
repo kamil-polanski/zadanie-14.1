617a99d466af6abb36fa1ab45a39740b729286af
Kamil Polanski <polanski88@gmail.com>
2019-05-08 23:55:00 +0200

    Hide dots, Add scroll bar

--- js/script.js
+++ js/script.js
@@ -1,7 +1,16 @@
 var elem = document.querySelector('.main-carousel');
 var flkty = new Flickity(elem, {
-
+    pageDots: false,
     cellAlign: 'left',
-    contain: true,
-    fullscreen: true
+    fullscreen: true,
+
+
+
+});
+
+var progressBar = document.querySelector('.progress-bar')
+
+flkty.on('scroll', function(progress) {
+    progress = Math.max(0, Math.min(1, progress));
+    progressBar.style.width = progress * 100 + '%';
 });
