c64fdd7610200f93f13e2a04444b17767cbf6904
Kamil Polanski <polanski88@gmail.com>
2019-05-11 13:15:35 +0200

    Add const declaration instead var

--- js/script.js
+++ js/script.js
@@ -1,21 +1,21 @@
-var flkty = new Flickity('.carousel', {
+const flkty = new Flickity('.carousel', {
     hash: true,
     pageDots: false,
     cellAlign: 'left',
     lazyLoad: 2,
 });
 
-var progressBar = document.querySelector('.progress-bar');
+const progressBar = document.querySelector('.progress-bar');
 
 flkty.on('scroll', function(progress) {
     progress = Math.max(0, Math.min(1, progress));
     progressBar.style.width = progress * 100 + '%';
 });
 
-var button = document.querySelectorAll('.reset');
+const button = document.querySelectorAll('.reset');
 
 for (ele of button) {
-    ele.addEventListener("click", function() {
+    ele.addEventListener('click', function() {
         flkty.select(0);
     });
 }
