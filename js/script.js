6ef9ab85f5c817e5027e4d6b426d73289a8b184a
Kamil Polanski <polanski88@gmail.com>
2019-05-11 22:34:41 +0200

    Add template mustache for slides

--- js/script.js
+++ js/script.js
@@ -1,21 +1,65 @@
-var flkty = new Flickity('.carousel', {
+const data = [{
+        title: `Odcinek AMC`,
+        img: `images/DSC_0137.JPG`,
+        desciption: `linii DISAMATIC`,
+        id: `1`
+    },
+    {
+        title: `OTTO Junker`,
+        img: `images/DSC_0257.JPG`,
+        desciption: `zalewarka`,
+        id: `2`
+    },
+    {
+        title: `Formierka HWS`,
+        img: `images/DSC_0262.JPG`,
+        desciption: ``,
+        id: `3`
+    },
+    {
+        title: `Linia HWS`,
+        img: `images/DSC_0268.JPG`,
+        desciption: `składarka`,
+        id: `4`
+    },
+    {
+        title: `ABB OCC40`,
+        img: `images/DSC_0273.JPG`,
+        desciption: `zalewarka`,
+        id: `5`
+    },
+
+];
+(function() {
+    const output = document.querySelector(`#template`).innerHTML;
+    Mustache.parse(output);
+    let generated = ``;
+    for (var i = 0; i < data.length; i++) {
+        generated += Mustache.render(output, data[i]);
+        console.log(generated);
+    }
+    const results = document.querySelector(`#results`);
+    results.insertAdjacentHTML(`beforeend`, generated);
+})();
+
+const flkty = new Flickity(`.carousel`, {
     hash: true,
     pageDots: false,
-    cellAlign: 'left',
-    lazyLoad: 2,
+    cellAlign: `left`,
+    lazyLoad: 2
 });
 
-var progressBar = document.querySelector('.progress-bar');
+const progressBar = document.querySelector(`.progress-bar`);
 
-flkty.on('scroll', function(progress) {
+flkty.on(`scroll`, function(progress) {
     progress = Math.max(0, Math.min(1, progress));
-    progressBar.style.width = progress * 100 + '%';
+    progressBar.style.width = progress * 100 + `%`;
 });
 
-var button = document.querySelectorAll('.reset');
+const button = document.querySelectorAll(`.reset`);
 
 for (ele of button) {
-    ele.addEventListener("click", function() {
+    ele.addEventListener(`click`, function() {
         flkty.select(0);
     });
 }
