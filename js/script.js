a666af57ec8c6ecf99deecd1fba59e0a11edf24b
Kamil Polanski <polanski88@gmail.com>
2019-05-12 12:24:58 +0200

    Fix script.js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 (function() {
     window.initMap = function() {
-        var uluru = { lat: -25.363, lng: 131.044 };
-        var map = new google.maps.Map(document.getElementById('map'), {
+        const uluru = { lat: -25.363, lng: 131.044 };
+        const map = new google.maps.Map(document.querySelector('#map'), {
             zoom: 4,
             center: uluru
         });
-        var marker = new google.maps.Marker({
+        const marker = new google.maps.Marker({
             position: uluru,
             map: map
         });
